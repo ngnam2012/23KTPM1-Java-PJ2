93dfce7a53c6724c50a787e604c1b39881dbbb28
ngnam2012 <nam20122005@gmail.com>
2025-12-11 20:16:59 +0700

    create project

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.example._3ktpm1javapj2 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.example._3ktpm1javapj2 to javafx.fxml;
+    exports com.example._3ktpm1javapj2;
+}
