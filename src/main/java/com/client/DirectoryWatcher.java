c7a3241cb47f251b290286b77ee21ccd2ec09f7e
ngnam2012 <nam20122005@gmail.com>
2025-12-12 21:53:45 +0700

    update client/DirectoryWatcher

--- src/main/java/com/client/DirectoryWatcher.java
+++ src/main/java/com/client/DirectoryWatcher.java
@@ -1,4 +1,23 @@
 package com.client;
 
-public class DirectoryWatcher {
+import com.common.*;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.time.LocalDateTime;
+import java.util.*;
+import java.util.function.*;
+public class DirectoryWatcher implements Runnable {
+    private final Path watchDirectory;
+    private final Consumer<FileChangeEvents> onChange;
+    private final String clientId;
+    public DirectoryWatcher(String directory, String clientId, Consumer<FileChangeEvents> onChange ) {
+        this.watchDirectory = Paths.get(directory);
+        this.clientId = clientId;
+        this.onChange = onChange;
+    }
+    @Override
+    public void run() {
+
+    }
 }
